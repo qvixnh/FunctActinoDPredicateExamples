fee67d27b9d1d1c329eeb386c717308ce17a29cd
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-31 14:26:47 +0700

    Delegate methods with generic parameter

--- FunctActinoDPredicateExamples/Program.cs
+++ FunctActinoDPredicateExamples/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.ComponentModel;
+
 namespace FuncActionPredicateExamples
 {
     class Program
     {
+        delegate TResult Func2<out TResult>();
+        delegate TResult Func2<in T1, TResult>(T1 arg);
+        delegate TResult Func2<in T1, in T2, TResult>(T1 arg1, T2 arg2);
+        delegate TResult Func2<in T1, in T2, in T3, TResult>(T1 arg1,T2 arg2, T3 arg3);
+
+
         static void Main(string[] args) {
-            MathClass mathClass = new MathClass();
-            Func<int, int, int> calc = mathClass.Sum;
-            //Func<int, int ,int > calc = (a,b) => a+b;
-            int result = calc(1, 2);
-            Console.WriteLine($"Result: {result}");
+            float d= 2.3f, e = 4.56f;
+            int f = 5;
+            Func<float, float, int, float> calc2 = (arg1, arg2, arg3) => (arg1 + arg2) * 3;
+            float result2 = calc2(d, e, f);
+            Console.WriteLine($"Result: {result2}");
             Console.ReadKey();
         }
     }
