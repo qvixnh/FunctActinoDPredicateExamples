a7954b968a69c54a99e2a5676b3dbcdc3ad18fbc
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-31 14:15:48 +0700

    basic Func generic delegate method for calculate the sum

--- FunctActinoDPredicateExamples/Program.cs
+++ FunctActinoDPredicateExamples/Program.cs
@@ -3,7 +3,21 @@ namespace FuncActionPredicateExamples
 {
     class Program
     {
-        static void Main(string[] args) { 
+        static void Main(string[] args) {
+            MathClass mathClass = new MathClass();
+            Func<int, int, int> calc = mathClass.Sum;
+            //Func<int, int ,int > calc = (a,b) => a+b;
+            int result = calc(1, 2);
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+    public class MathClass
+    {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
